c0aa62008b9ab8b821cb3879ab5f8e443209fa7b
V.Devanarayanan <vdcoder69@gmail.com>
2025-06-03 23:35:12 +0530

    hyperlinks working

--- app.py
+++ app.py
@@ -8,5 +8,40 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/instagram')
+def instagram():
+    return render_template('index3.html')
+
+
+@app.route('/pinterest')
+def pinterest():
+    return render_template('index4.html')
+
+
+@app.route('/reddit')
+def reddit():
+    return render_template('index5.html')
+
+
+@app.route('/tiktok')
+def tiktok():
+    return render_template('index6.html')
+
+
+@app.route('/twitch')
+def twitch():
+    return render_template('index7.html')
+
+
+@app.route('/twitter')
+def twitter():
+    return render_template('index8.html')
+
+
+@app.route('/youtube')
+def youtube():
+    return render_template('index9.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
